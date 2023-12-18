6857343e6d2639e88cff5d268d495b8b4f97c944
RoduTensei <senpaiyshi@gmail.com>
2023-12-18 20:59:33 +0800

    e

---
+++ index.js
@@ -0,0 +1,13 @@
+let prevScrollPos = window.pageYOffset;
+
+window.onscroll = function() {
+  const currentScrollPos = window.pageYOffset;
+  
+  if (prevScrollPos > currentScrollPos) {
+    document.getElementById("navbar").style.top = "0";
+  } else {
+    document.getElementById("navbar").style.top = "-90px"; 
+  }
+
+  prevScrollPos = currentScrollPos;
+};
