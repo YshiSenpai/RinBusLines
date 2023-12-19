f593e59d38c7f8511e20b09a7e6114508ad39693
RoduTensei <senpaiyshi@gmail.com>
2023-12-20 02:34:59 +0800

    hamburger bar

--- index.js
+++ index.js
@@ -12,5 +12,16 @@ window.onscroll = function() {
   prevScrollPos = currentScrollPos;
 };
 
+const navEl = document.querySelector(".navR");
+const BEl = document.querySelector(".burger");
 
+BEl.addEventListener("click", () => {
+  navEl.classList.toggle(".nav--open");
+  BEl.classList.toggle(".burger.active");
+});
+
+BEl.addEventListener("click", () => {
+  navEl.classList.remove(".nav--open");
+  BEl.classList.remove(".burger.active");
+});
 
